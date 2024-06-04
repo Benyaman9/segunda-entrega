f496f3b30f309e085ad16ea98e9fd9cfc92b0962
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2024-06-04 18:24:40 -0300

    segunda-entrega

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        img: 'https://th.bing.com/th/id/OIP.vTsFYF_c_JqsYqi6c-GNQgHaHa?w=207&h=207&c=7&r=0&o=5&pid=1.7',
+        nameProduct: 'Zapatillas',
+        price: 80,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: 'https://th.bing.com/th/id/OIP.Ih8wvE26fZh23zHROeOmLwHaHI?rs=1&pid=ImgDetMain',
+        nameProduct: 'Reloj',
+        price: 60,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+        nameProduct: 'Smartwatch',
+        price: 90,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: 'https://th.bing.com/th/id/OIP.THCD6G5od-g2VvcQRbr5BgHaHa?rs=1&pid=ImgDetMain',
+        nameProduct: 'Perfume',
+        price: 50,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: 'https://th.bing.com/th/id/OIP.0QPHii6HcE8omIYm1WxqhwHaHa?rs=1&pid=ImgDetMain',
+        nameProduct: 'Auriculares gamer',
+        price: 30,
+        quantity: 1,
+    },
+    // Aquí pueden agregar más o modificar los productos existentes
+];
